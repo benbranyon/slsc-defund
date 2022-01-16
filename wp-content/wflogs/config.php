0e3ccf19d01866620b81cbb990e83c43a789ff08
Ben <benbranyon@gmail.com>
2022-01-15 20:16:18 -0500

    updates

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1642900146;s:7:"authKey";s:64:"s%%8xg{g! /!jbbFVXRTIErNrp*&eJ,Rv_fm)kIlL>4UUfb|$NX&$uSdA]2ob(g1";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:800;}
